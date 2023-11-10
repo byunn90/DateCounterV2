cc500e455cad54e7ac673142442b2d37c7d139df
byunn90 <kayhanturk90@Live.com>
2023-11-10 19:34:16 +1100

    working on my date counter

---
+++ date-counter/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  return <div></div>;
+}
+
+function Counter() {
+  // const [step, setStep] = useState(1);
+  // const [count, SetCount] = usestate(1);
+
+  return (
+    <div>
+      {/* <button onClick={()}></button>
+      <button onClick={()}></button> */}
+    </div>
+  );
+}
